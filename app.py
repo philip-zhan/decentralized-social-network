838e31c18ea6289d20363ea54920c3be48ab21f6
Juejie Pan <j33pan@uwaterloo.ca>
2018-07-16 21:35:28 -0400

    add a Block class

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from hashlib import sha256
+import json
 
 app = Flask(__name__)
+
+class Block(object):
+	"""docstring for Block"""
+	def __init__(self, index, transactions, timestamp):
+		super(Block, self).__init__()
+		self.index = index
+		self.transactions = transactions
+		self.timestamp = timestamp
+
+	def compute_hash(block):
+		block_string = json.dumps(self.__dict__, sort_keys=True)
+		return sha256(block_string.encode()).hexdigest()
+
+		
+
 
 
 @app.route('/')
