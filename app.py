619c716b84ef7b3776c9bc099fa78cb83741d71f
Juejie Pan <j33pan@uwaterloo.ca>
2018-07-16 22:46:09 -0400

    implement the interface w/o concensus

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from hashlib import sha256
 import json
 import time
 
-app = Flask(__name__)
-
 class Block(object):
 	def __init__(self, index, transactions, timestamp, previous_hash):
 		super(Block, self).__init__()
@@ -78,11 +76,51 @@ class Blockchain(object):
 		self.add_block(new_block, proof)
 
 		self.unconfirmed_transactions = []
-		return new_block.index
+		return new_block
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ["author", "content"]
+ 
+    for field in required_fields:
+        if not tx_data.get(field):
+            return "Invlaid transaction data", 404
+ 
+    tx_data["timestamp"] = time.time()
+ 
+    blockchain.add_new_transaction(tx_data)
+ 
+    return "Success", 201
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+    if not result:
+        return "No transactions to mine"
+    return "Block #{} is mined.".format(result)
+ 
+ 
+# endpoint to query unconfirmed transactions
+@app.route('/pending_tx')
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
 
 if __name__ == '__main__':
